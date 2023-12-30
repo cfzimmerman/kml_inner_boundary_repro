dc9d784c7ce9eb4eae93a9ac1e870e375247d7f9
cfzimmerman <coryzimmerman93@gmail.com>
2023-12-30 17:23:32 -0600

    reproduce inner_boundary parsing issue

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a minimum reproduction of a parsing oversight currently in the Rust KML crate v0.8.4.
+
+The contained KML file is not proprietary.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use kml::KmlReader;
+
+// parsed_kml.txt generated with `cargo run > parsed_kml.txt`
 fn main() {
-    println!("Hello, world!");
+    let mut kml_reader = KmlReader::<_, f64>::from_path("./inner_boundaries.kml").unwrap();
+    let kml = kml_reader.read().unwrap();
+    println!("{:#?}", kml);
 }
