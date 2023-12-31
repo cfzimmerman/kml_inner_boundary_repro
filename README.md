fb1c5761c060b3cdaad6939eb5d4597f18612a6d
cfzimmerman <coryzimmerman93@gmail.com>
2023-12-30 18:11:39 -0600

    add visualization for proposed changes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is a minimum reproduction of a parsing oversight currently in the Rust KML crate v0.8.4.
 
-The contained KML file is not proprietary.
+- The contained KML file is not proprietary.
+- The `.txt` files contain the output of parsing the KML file with the current `kml` crate and my proposed addition.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-use kml::KmlReader;
+use kml::KmlReader as CurrKmlReader;
+use kml_proposed::KmlReader as ProposedKmlReader;
 
-// parsed_kml.txt generated with `cargo run > parsed_kml.txt`
+// Output files generated with:
+// cargo run -- curr > curr_parser.txt
+// cargo run -- proposed > proposed_parser.txt
 fn main() {
-    let mut kml_reader = KmlReader::<_, f64>::from_path("./inner_boundaries.kml").unwrap();
-    let kml = kml_reader.read().unwrap();
-    println!("{:#?}", kml);
+    let args: Vec<String> = std::env::args().collect();
+    let version: &String = args.get(1).expect("Requires argument `curr` or `proposed`");
+
+    match version.as_str() {
+        "curr" => {
+            let mut curr_kml_reader =
+                CurrKmlReader::<_, f64>::from_path("./inner_boundaries.kml").unwrap();
+            let kml_curr = curr_kml_reader.read().unwrap();
+            println!("{:#?}", kml_curr);
+        }
+        "proposed" => {
+            let mut proposed_kml_reader =
+                ProposedKmlReader::<_, f64>::from_path("./inner_boundaries.kml").unwrap();
+            let kml_proposed = proposed_kml_reader.read().unwrap();
+            println!("{:#?}", kml_proposed);
+        }
+        _ => panic!("Argument must be either `curr` or `proposed`"),
+    }
 }
